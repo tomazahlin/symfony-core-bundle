5ca59057621632bb729c63867ed9d97ae8856219
Tomaz Ahlin <tahlin@sessiondigital.de>
2015-03-30 12:52:11 +0200

    Fixed data transformer exception instance, and some logic

--- src/Ahlin/Bundle/CoreBundle/Form/DataTransformer/DateTimeToIso8601Transformer.php
+++ src/Ahlin/Bundle/CoreBundle/Form/DataTransformer/DateTimeToIso8601Transformer.php
@@ -4,30 +4,16 @@ namespace Ahlin\Bundle\CoreBundle\Form\DataTransformer;
 
 use DateTime;
 use Exception;
-use Ahlin\Bundle\CoreBundle\Exception\FormValidationException;
 use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
 
 class DateTimeToIso8601Transformer implements DataTransformerInterface
 {
     /**
-     * @var string
-     */
-    private $formFieldName;
-
-    /**
-     * Constructor to set which form field should contain an error message if transformer fails
-     * @param $formFieldName
-     */
-    function __construct($formFieldName)
-    {
-        $this->formFieldName = $formFieldName;
-    }
-
-    /**
      * Transforms an ISO8601 string to DateTime object.
      * @param  string $iso
      * @return DateTime
-     * @throws FormValidationException
+     * @throws TransformationFailedException
      */
     public function reverseTransform($iso)
     {
@@ -35,8 +21,7 @@ class DateTimeToIso8601Transformer implements DataTransformerInterface
             return new DateTime($iso);
         }
         catch (Exception $e) {
-            $exception = new FormValidationException();
-            $exception->setError($this->formFieldName, 'Not an ISO8601 format.');
+            $exception = new TransformationFailedException();
             throw $exception;
         }
     }
@@ -45,15 +30,19 @@ class DateTimeToIso8601Transformer implements DataTransformerInterface
      * Transformation will return ISO8601 format from the datetime
      * @param  DateTime $datetime
      * @return string
-     * @throws FormValidationException
+     * @throws TransformationFailedException
      */
     public function transform($datetime)
     {
+        if (empty($datetime)) {
+            return '';
+        }
+
         if ($datetime instanceof DateTime) {
             return $datetime->format(DateTime::ISO8601);
         }
-        $exception = new FormValidationException();
-        $exception->setError($this->formFieldName, 'Not a DateTime object.');
+
+        $exception = new TransformationFailedException();
         throw $exception;
     }
 }
